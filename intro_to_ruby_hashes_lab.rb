a52262753ca232c7d229b8a07d730bf66c979a3e
Kierra M <k.merriman32@hotmail.com>
2019-08-07 22:23:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ my_hash_creator = {key => value}
 end
 
 def read_from_hash(hash, key)
- read_from_hash:hash => key}
+ read_from_hash [:hash]
+ read_from_hash [:key]
 end
 
 def update_counting_hash(hash, key)
